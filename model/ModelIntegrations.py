0c111b70273d15525bb9dedec60504f5c60e7136
cd@bziiit.com <cyril.dupland@gmail.com>
2024-10-22 09:51:42 +0200

    fiw deprecation warning on ChatOpenAI

--- model/ModelIntegrations.py
+++ model/ModelIntegrations.py
@@ -2,7 +2,7 @@ import os
 
 from .ModelStrategy import ModelStrategy
 
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_mistralai.chat_models import ChatMistralAI
 from langchain_anthropic import ChatAnthropic
 
