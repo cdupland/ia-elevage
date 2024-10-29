b440ebfcf2c8d4da3747b116a13ea53ddf16dd50
cd@bziiit.com <cyril.dupland@gmail.com>
2024-10-29 16:59:51 +0100

    feat: update config.yaml and form.py to support new input types for parameters

--- pages/form.py
+++ pages/form.py
@@ -5,10 +5,20 @@ def page():
 
     # Boucle pour créer des inputs basés sur data_dict (qui est une liste ici)
     for param in st.session_state.data_dict:
-        # Utilisation de la clé 'label' et 'value' pour afficher et récupérer les valeurs
-        value = st.text_input(label=param['label'], value=param['value'] if param['value'] else "")
         
-        # Mettre à jour la valeur dans le dictionnaire après la saisie utilisateur
-        param['value'] = value
+        if param['nature'] == 'radio':
+          value = st.radio(param['label'], param['options'], key=param['key'], args=[param['key']])
+          param['value'] = value
+        elif param['nature'] == 'selectbox':
+          value = st.selectbox(param['label'], param['options'], key=param['key'], args=[param['key']])
+          param['value'] = value
+        elif param['nature'] == 'multiselect':
+          value = st.multiselect(param['label'], param['options'], key=param['key'], args=[param['key']])
+          param['value'] = value
+        else:
+          # Utilisation de la clé 'label' et 'value' pour afficher et récupérer les valeurs
+          value = st.text_input(label=param['label'], value=param['value'] if param['value'] else "")
+          # Mettre à jour la valeur dans le dictionnaire après la saisie utilisateur
+          param['value'] = value
 
 page()
